216deb291780b1575d06f1efa28bba13752dc0f7
yprateek136 <yprateek195@gmail.com>
2020-08-08 11:19:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# complete_web_developement
+ Text analyzer 
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/prateek/complete_web_developement/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
